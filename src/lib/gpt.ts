6b5f899a9dcad9f8db1f5945cc756f075a9f729e
Robert Greenwood <RobertGreenwood27@gmail.com>
2023-11-19 15:59:48 -0700

    updated gpt5

--- src/lib/gpt.ts
+++ src/lib/gpt.ts
@@ -1,23 +1,29 @@
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from 'openai';
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
 });
-
-const openai = new OpenAIApi(configuration);
 
 export const getGPT3Response = async (messages, res) => {
   try {
-    const completion = await openai.createChatCompletion({
+    const chatCompletion = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: messages,
     });
 
-    const content = completion.data.choices[0].message.content.trim();
+    const content = chatCompletion.choices[0].message.content.trim();
     res.write(content);
     res.end();
   } catch (error) {
-    console.error(error);
+    if (error instanceof OpenAI.APIError) {
+      console.error(error.status);  // e.g. 401
+      console.error(error.message); // e.g. The authentication token you passed was invalid...
+      console.error(error.code);    // e.g. 'invalid_api_key'
+      console.error(error.type);    // e.g. 'invalid_request_error'
+    } else {
+      // Non-API error
+      console.log(error);
+    }
     res.status(500).json({ error: "An error occurred during your request." });
   }
 };
