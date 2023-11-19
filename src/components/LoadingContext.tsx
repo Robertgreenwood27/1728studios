ee166ed4d10a38a8b0ce697286156a79a8e55562
Robert Greenwood <RobertGreenwood27@gmail.com>
2023-11-19 10:48:45 -0700

    updated loadingcontext

--- src/components/LoadingContext.tsx
+++ src/components/LoadingContext.tsx
@@ -1,17 +1,25 @@
 import React, { createContext, useContext, useState } from 'react';
 
-// Create the context
-const LoadingContext = createContext({
+// Define the context type
+type LoadingContextType = {
+  isLoading: boolean;
+  setLoading: (state: boolean) => void;
+  children?: React.ReactNode; // Adding this line to include children
+};
+
+// Create the context with an initial value
+const LoadingContext = createContext<LoadingContextType>({
   isLoading: false,
-  setLoading: (state: boolean) => {}
+  setLoading: () => {},
+  // children is optional and doesn't need an initial value
 });
 
 // Provide the context
-export const LoadingProvider: React.FC = ({ children }) => {
+export const LoadingProvider: React.FC<LoadingContextType> = ({ children, ...rest }) => {
   const [isLoading, setLoading] = useState(false);
 
   return (
-    <LoadingContext.Provider value={{ isLoading, setLoading }}>
+    <LoadingContext.Provider value={{ isLoading, setLoading, ...rest }}>
       {children}
     </LoadingContext.Provider>
   );
