43d9c2bdd2525133f09e9b6b18235a4d4c613008
Robert Greenwood <RobertGreenwood27@gmail.com>
2023-11-19 11:40:31 -0700

    removed custom comps from charcard

--- src/components/characterCard.tsx
+++ src/components/characterCard.tsx
@@ -1,41 +1,36 @@
 import React, { useMemo } from 'react';
-import builder from '../lib/imageUrlBuilder';
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
 import Link from 'next/link';
 
 const CharacterCard = ({ teacher }) => {
-  const imageUrl = builder
-    .image(teacher.photo)
-    .width(200)
-    .height(200)
-    .auto('format')
-    .fit('crop')
-    .url();
+  const imageUrl = useMemo(() => {
+    // I'm assuming you have a method to build the image URL. 
+    // Replace this with your actual method if different.
+    return `/path/to/images/${teacher.photo}`;
+  }, [teacher.photo]);
 
   const randomBgNumber = useMemo(() => Math.floor(Math.random() * 10) + 1, []);
   const bgImagePath = `/bg${randomBgNumber}.png`;
 
   return (
-    // Use the teacher's slug for the URL
     <Link href={`/teacher/${teacher.slug?.current}`}>
-        <Card className="flex flex-col items-center transform hover:scale-105 transition-transform duration-300 border-blue-800 rounded-xl">
-          <CardHeader className="flex flex-col items-center">
-            <div 
-              className="rounded-full overflow-hidden"
-              style={{
-                backgroundImage: `url('${bgImagePath}')`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-              }}
-            >
-              <img src={imageUrl} alt={teacher.name} className="max-w-[200px] max-h-[200px]" />
-            </div>
-            <CardTitle>{teacher.name}</CardTitle>
-          </CardHeader>
-          <CardContent className="flex flex-col items-center">
-            <CardDescription>{teacher.departmentOrSubject}</CardDescription>
-          </CardContent>
-        </Card>
+      <div className="card flex flex-col items-center transform hover:scale-105 transition-transform duration-300 border-blue-800 rounded-xl">
+        <div className="card-header flex flex-col items-center">
+          <div 
+            className="rounded-full overflow-hidden"
+            style={{
+              backgroundImage: `url('${bgImagePath}')`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          >
+            <img src={imageUrl} alt={teacher.name} className="max-w-[200px] max-h-[200px]" />
+          </div>
+          <div className="card-title">{teacher.name}</div>
+        </div>
+        <div className="card-content flex flex-col items-center">
+          <div className="card-description">{teacher.departmentOrSubject}</div>
+        </div>
+      </div>
     </Link>
   );
 };
