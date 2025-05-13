39b57130b12f7fbe5238b57e6b10866500202e1b
RobertGreenwood27@gmail.com <RobertGreenwood27@gmail.com>
2025-05-12 19:43:48 -0600

    &apos;

--- src/pages/consulting.tsx
+++ src/pages/consulting.tsx
@@ -5,7 +5,7 @@ const ConsultingPage = () => {
     <div className="min-h-screen flex items-center justify-center bg-black text-white">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">Consulting Info Coming Soon</h1>
-        <p className="text-lg text-gray-300">We're working hard to provide you with the best consulting services information. Stay tuned!</p>
+        <p className="text-lg text-gray-300">We&apos;re working hard to provide you with the best consulting services information. Stay tuned!</p>
       </div>
     </div>
   );
